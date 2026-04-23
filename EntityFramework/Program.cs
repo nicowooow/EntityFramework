caed239bee6800102c5cf979d39b2c4dfd83818c
nicowooow <nicolasgianfranco123@gmail.com>
2026-04-23 09:50:27 +0200

    agregamos los datos para insertarlos enestudantes, tanto cursos, profesores y alumnos

--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -46,10 +46,27 @@ class Program
         
         var estudiantesCurso = DBContext.Estudiantes.Include(i=> i.Curso).Where(e => e.Curso.Nombre == "Curso1");
         
-        foreach (var estudiante in estudiantesCurso)
+        foreach (var estudiante_curso in estudiantesCurso)
         {
-            Console.WriteLine($"estudiante {estudiante.Id} - {estudiante.Nombre} - {estudiante.CursoId}");
+            Console.WriteLine($"estudiante {estudiante_curso.Id} - {estudiante_curso.Nombre} - {estudiante_curso.CursoId}");
         }
+        
+        var Profesor1 = new Profesor() { Nombre =  "luis" , Apellidos = "rodrigez" };
+        var Profesor2 = new Profesor() { Nombre =  "maria" , Apellidos = "magdalena" };
+        var Curso1 = new Curso{Nombre = "Matematicas", Profesores = [Profesor1]};
+        var Curso2 = new Curso(){Nombre = "Lengua", Profesores =  [Profesor2]};
+
+        var estudiante1 = new Estudiante { Nombre = "A", Apellido = "A", DireccionId = 0,Curso =  Curso1 };
+        var estudiante2 = new Estudiante { Nombre = "B", Apellido = "B", DireccionId = 0,Curso =  Curso2 };
+        var estudiante3 = new Estudiante { Nombre = "C", Apellido = "C", DireccionId = 0,Curso =  Curso2 };
+        var estudiante4 = new Estudiante { Nombre = "D", Apellido = "D", DireccionId = 0,Curso =  Curso1 };
+        
+        DBContext.Estudiantes.Add(estudiante1);
+        DBContext.Estudiantes.Add(estudiante2);
+        DBContext.Estudiantes.Add(estudiante3);
+        DBContext.Estudiantes.Add(estudiante4);
+        DBContext.SaveChanges();
+
 
     }
 }
